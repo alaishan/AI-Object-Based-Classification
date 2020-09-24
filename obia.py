992bf07f4e2bc8dae74064e45612ee45eaea4dfc
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2020-09-24 23:56:46 +0200

    Add files via upload

--- obia.py
+++ obia.py
@@ -16,11 +16,11 @@ sentinel2_fn = "#enter pathname"
 driverTiff = gdal.GetDriverByName("GTiff")
 sentinel2_ds = gdal.Open(sentinel2_fn)
 
-nbands = sentinel2_ds.RasterCount() #number of bands of imagery
+nbands = sentinel2_ds.RasterCount #number of bands of imagery
 
 band_data = [] #empty list
 
-print('bands', sentinel2_ds.RaterCount(), 'rows', sentinel2_ds.RaterYSize(), 'coloumns', sentinel2_ds.RaterXSize())
+print('bands', sentinel2_ds.RaterCount, 'rows', sentinel2_ds.RaterYSize(), 'coloumns', sentinel2_ds.RaterXSize())
 
 for i in range(1, nbands+1): #loop through bands and save data from 1 band to nbands +1
     band = sentinel2_ds.GetRasterBand(i).ReadAsArray() #reads data as a numpy array
